f4cb736f70b6d73c5b036c4d4473fdbe833d6cb1
ChillyPine <chillypines@gmail.com>
2026-05-30 20:19:05 -0600

    Added logic for batesp to not work on wither doors

--- src/main/kotlin/io/github/meridian/features/impl/dungeons/BatFeatures.kt
+++ src/main/kotlin/io/github/meridian/features/impl/dungeons/BatFeatures.kt
@@ -5,6 +5,7 @@ import io.github.meridian.features.ColorFeature
 import io.github.meridian.features.SwitchFeature
 import io.github.meridian.utils.ESP
 import net.fabricmc.fabric.api.client.rendering.v1.world.WorldRenderEvents
+import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.ambient.Bat
 
 // Bat esp
@@ -21,11 +22,15 @@ object BatESP : SwitchFeature (
             if (!enabled) return@register
             val level = Meridian.mc.level ?: return@register
             val player = Meridian.mc.player ?: return@register
-            for (ent in level.entitiesForRendering()) {
-                if (ent !is Bat) continue
-                if (ent.distanceToSqr(player.x, player.y, player.z) > 100 * 100) continue
-                ESP.drawBox(ctx, ent, w = 0.5, h = 1.0, wz = 0.5, yOffset = 0.0, argb = BatESPColor.color)
-
+            val bats = level.entitiesForRendering().filterIsInstance<Bat>()
+            for (bat in bats) {
+                if (bat.distanceToSqr(player.x, player.y, player.z) > 100 * 100) continue
+                // Fuck hypixel wither/blood doors
+                val nearbyBat = bats.any { other ->
+                    other.id != bat.id && other.distanceToSqr(bat.x, bat.y, bat.z) <= 3.0 * 3.0
+                }
+                if (nearbyBat) continue
+                ESP.drawBox(ctx, bat, w = 0.5, h = 1.0, wz = 0.5, yOffset = 0.0, argb = BatESPColor.color)
             }
         }
     }
