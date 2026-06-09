4f17128692769aef292a4b7d772cd33d14d47008
ChillyPine <chillypines@gmail.com>
2026-06-09 17:29:46 -0600

    Changed something, that's for sure...

--- src/main/kotlin/io/github/meridian/features/impl/dungeons/BottleFull.kt
+++ src/main/kotlin/io/github/meridian/features/impl/dungeons/BottleFull.kt
@@ -9,7 +9,6 @@ import io.github.meridian.utils.onChatMessage
 import net.minecraft.client.resources.sounds.SimpleSoundInstance
 import net.minecraft.sounds.SoundEvents
 
-// Notifies of storm bottle full (and variants)
 object BottleFull : SwitchFeature(
     name = "Bottle Full",
     description = "Notifies you of filled bottles after a dungeon",
@@ -20,25 +19,24 @@ object BottleFull : SwitchFeature(
     private val TEAM_SCORE = Regex("^ *Team Score: (\\d+) \\(([\\w+]{1,2})\\)$")
 
     init {
-
         onChatMessage { text, _, _ ->
             if (!enabled) return@onChatMessage
             if (!TEAM_SCORE.matches(text)) return@onChatMessage
 
             val bottles = mapOf(
-                "Thunder in a Bottle"    to "Your Thunder Bottle is §aFull! §eNice!",
-                "Storm in a Bottle"      to "Your Storm Bottle is §aFull! §dImpressive!",
-                "Hurricane in a Bottle"  to "Your Hurricane Bottle is §aFull! §6WOW!",
+                "Thunder in a Bottle" to "Your Thunder Bottle is §aFull! §eNice!",
+                "Storm in a Bottle" to "Your Storm Bottle is §aFull! §dImpressive!",
+                "Hurricane in a Bottle" to "Your Hurricane Bottle is §aFull! §6WOW!",
             )
 
             bottles.forEach { (item, message) ->
-                if (hasItem(item))
+                if (hasItem(item)) {
                     TickScheduler.schedule(5, serverTick = false) {
                         mc.soundManager.play(SimpleSoundInstance.forUI(SoundEvents.FIREWORK_ROCKET_LAUNCH, 1.0f))
                         modMessage(message)
                     }
+                }
             }
         }
-
     }
 }
