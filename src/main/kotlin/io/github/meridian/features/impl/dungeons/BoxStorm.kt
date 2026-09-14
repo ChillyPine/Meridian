1bf8696173324e28efe209300621722d9ee9c300
DwnInFraggleRock <ethan@tibre.org>
2026-09-14 01:52:02 -0700

    Interal Fix: CLean up and use proper state checker.

--- src/main/kotlin/io/github/meridian/features/impl/dungeons/BoxStorm.kt
+++ src/main/kotlin/io/github/meridian/features/impl/dungeons/BoxStorm.kt
@@ -4,6 +4,7 @@ import io.github.meridian.Meridian
 import io.github.meridian.features.types.ColorFeature
 import io.github.meridian.features.types.SwitchFeature
 import io.github.meridian.utils.ESP
+import io.github.meridian.utils.P2State
 import io.github.meridian.utils.onChatMessage
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.minecraft.client.multiplayer.ClientLevel
@@ -16,35 +17,15 @@ object BoxStorm : SwitchFeature(
     configKey = "box_storm",
     subcategory = "P2"
 ) {
-
-    @Volatile private var stormPhase = false
-    private var lastLevel: ClientLevel? = null
-
     init {
-        onChatMessage { text, _, _ ->
-            when (text) {
-                "[BOSS] Storm: Pathetic Maxor, just like expected." -> stormPhase = true
-                "[BOSS] Goldor: Who dares trespass into my domain?" -> stormPhase = false
-            }
-        }
-
-        // see world change
-        ClientTickEvents.END_CLIENT_TICK.register(ClientTickEvents.EndTick {
-            val level = Meridian.mc.level
-            if (level !== lastLevel) {
-                lastLevel = level
-                stormPhase = false
-            }
-        })
 
         onRender { ctx ->
-            if (!stormPhase) return@onRender
+            if (!P2State.inP2) return@onRender
             val level = Meridian.mc.level ?: return@onRender
 
             // render that invisible dumbfucker
             val storm = level.entitiesForRendering()
                 .firstOrNull { it is WitherBoss && !it.isInvisible } as? WitherBoss ?: return@onRender
-
             ESP.drawFilled(ctx, storm, argb = StormColor.color)
         }
     }
